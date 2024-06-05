b155574c63c926f26dea10ed3cb54a6c4373c194
Felix Amenya <kenyansafelix@gmail.com>
2024-06-06 00:59:01 +0300

    final

---
+++ app/src/main/java/com/kenyansa/weatherapp2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.kenyansa.weatherapp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.kenyansa.weatherapp2.ui.theme.WeatherApp2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+
+        enableEdgeToEdge()
+        setContent {
+            WeatherApp2Theme {
+               Surface(
+                   modifier = Modifier.fillMaxSize(),
+                   color = MaterialTheme.colorScheme.background
+               ) {
+                   WeatherPage(weatherViewModel)
+               }
+            }
+        }
+    }
+
+}
+
